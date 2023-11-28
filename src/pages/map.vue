aff685249a761b421146f79888508ec7aeb1018e
JhonGarc <Jhon61631@gmail.com>
2023-11-27 20:04:36 -0500

    puntos aleatorios funcional

--- src/pages/map.vue
+++ src/pages/map.vue
@@ -1,13 +1,54 @@
+<!-- <template>
+  <GoogleMap api-key="YOUR_GOOGLE_MAPS_API_KEY" style="width: 100%; height: 500px" :center="center" :zoom="13">
+    <CustomControl position="BOTTOM_CENTER">
+      <button class="custom-btn" @click="sayHi">👋</button>
+    </CustomControl>
+  </GoogleMap>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from "vue";
+import { GoogleMap, CustomControl } from "vue3-google-map";
+
+
+
+    const center = { lat: 35, lng: -95 };
+    const sayHi = () => alert("Hi!");
+
+
+</script>
+
+<style scoped>
+.custom-btn {
+  box-sizing: border-box;
+  background: white;
+  height: 40px;
+  width: 40px;
+  border-radius: 2px;
+  border: 0px;
+  margin: 10px;
+  padding: 0px;
+  font-size: 1.25rem;
+  text-transform: none;
+  appearance: none;
+  cursor: pointer;
+  user-select: none;
+  box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 4px -1px;
+  overflow: hidden;
+}
+</style> -->
 
 <script setup lang="ts">
 import { GoogleMap, Marker } from 'vue3-google-map'
 import { db } from '@/services/firebase'
-import { collection, getDocs, query } from 'firebase/firestore'
-import { onMounted, ref,computed } from 'vue'
+import { collection, getDocs } from 'firebase/firestore'
+import { onMounted, ref, computed } from 'vue'
 
 import { PuntoXy } from '@/typings'
 
 const puntosxy = ref<PuntoXy[]>([]);
+const puntosF = ref<PuntoXy[]>([]);
+const locations = ref<{ lat: number; lng: number }[]>([]); // Mover la declaración aquí
 
 const latitud = ref(0)
 const longitud = ref(0)
@@ -15,39 +56,66 @@ const longitud = ref(0)
 async function getPuntos() {
   const colRef = collection(db, 'puntos');
   const querySnap = await getDocs(colRef);
-  
+
   querySnap.forEach((doc) => {
-  puntosxy.value.push(doc.data() as PuntoXy)
+    puntosxy.value.push(doc.data() as PuntoXy)
   });
   latitud.value = parseFloat(puntosxy.value[0].latitud)
   longitud.value = parseFloat(puntosxy.value[0].longitud)
-  
+
+  let indices = ref<number[]>([]);
+  while (indices.value.length < 4) {
+    let indicesA = Math.floor(Math.random() * (puntosxy.value.length - 1 - 0 + 1)) + 0;
+    if (!indices.value.includes(indicesA)) {
+      indices.value.push(indicesA);
+    }
+  }
+
+  indices.value.forEach((element) => console.log(element))
+
+  for (let i = 0; i < 4; i++) {
+    puntosF.value.push(puntosxy.value[indices.value[i]])
+  }
+
+
+  // Actualizar locations después de que puntosF.value se haya llenado
+  locations.value = [
+  { lat: parseFloat(puntosF.value[0].latitud), lng: parseFloat(puntosF.value[0].longitud) },
+  { lat: parseFloat(puntosF.value[1].latitud), lng: parseFloat(puntosF.value[1].longitud) },
+  { lat: parseFloat(puntosF.value[2].latitud), lng: parseFloat(puntosF.value[2].longitud) },
+  { lat: parseFloat(puntosF.value[3].latitud), lng: parseFloat(puntosF.value[3].longitud) },
+];
+
+  console.log('so' + locations.value);
 }
-
-
 
 onMounted(() => {
   getPuntos()
 })
 const center = computed(() => {
-  return { lat: latitud.value, lng: longitud.value };
+  return { lat: 4.1488976313196835, lng: -73.62845863205662 };
 });
 </script>
 
 <template>
-  <section class="h-1/5 flex">
-    <router-link class="bg-black rounded-lg h-1/3 w-20 my-10 mx-5" :to="{ name: 'index' }"
-      ><i class="pi pi-chevron-left mt-4" style="color: aliceblue"></i
-    ></router-link>
-  </section>
-  <GoogleMap
-    api-key="AIzaSyBhDknILN83Fc8eLama7Wr0ihN6nQUU-Aw"
-    style="width: 100%; height: 850px"
-    :center="center"
-    :zoom="15"
-  >
-    <Marker :options="{ position: center }" />
-  </GoogleMap>
+  <body 
+    class="fixed top-0 left-0 w-full h-full bg-cover bg-center"
+    style="background-image: url('/src/assets/images/bg.jpg')">
+    <section class="h-1/5 flex">
+      <router-link class="bg-black rounded-lg h-1/3 w-20 my-10 mx-5" :to="{ name: 'index' }">
+        <i class="pi pi-chevron-left mt-4" style="color: aliceblue"></i>
+      </router-link>
+    </section>
+    <GoogleMap
+      api-key="tuapikey"
+      style="width: 100%; height: 500px"
+      :center="center"
+      :zoom="7.4"
+    >
+      <Marker v-for="(location, i) in locations" :options="{ position: location }" :key="i" />
+    </GoogleMap>
+  </body>
 </template>
+
 
 
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,41 +1,75 @@
 <script setup lang="ts">
-import { db } from '@/services/firebase'
-import { collection, getDocs, query } from 'firebase/firestore'
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 
-import puntos from '@/components/Puntos.vue'
-
-import { PuntoXy } from '@/typings'
-
-const puntosxy = ref<PuntoXy[]>([]);
-
-async function getPuntos() {
-  const colRef = collection(db, 'puntos');
-  const querySnap = await getDocs(colRef);
-
-  querySnap.forEach((doc) => {
-  puntosxy.value.push(doc.data() as PuntoXy)
-  });
-  
-}
-
-
-onMounted(() => {
-  getPuntos()
+const formDataRegister = ref<{
+  email: string
+  password: string
+}>({
+  email: '',
+  password: '',
 })
-
-
 </script>
 
-
 <template>
-  <body class="fixed top-0 left-0 w-full h-full bg-cover bg-center" style="background-image: url('/src/assets/images/bg.jpg');">
-    <section class="h-1/5 flex">
-      <router-link class="bg-black rounded-lg h-1/3 w-20 my-10 mx-5" :to="{ name: 'index' }"><i class="pi pi-chevron-left mt-4" style="color: aliceblue;"></i></router-link>
+  <body
+    class="fixed top-0 left-0 w-full h-full bg-cover bg-center"
+    style="background-image: url('/src/assets/images/bg.jpg')"
+  >
+    <section class="flex bg-blue-100">
+      <router-link class="bg-black rounded-lg h-1/3 w-20 my-10 mx-5" :to="{ name: 'index' }"
+        ><i class="pi pi-chevron-left mt-4" style="color: aliceblue"></i
+      ></router-link>
     </section>
-    <div>
-      <puntos v-if="puntosxy?.length > 0" :elements="puntosxy"></puntos>
-      <span v-else>No points</span>
-    </div>
+    <section >
+      <div >
+        <form  @submit.prevent>
+        <div class="h-auto">
+          <div class="bg-red-200 flex flex-col justify-center -------------">
+          <div class="flex flex-col">
+            <label for="correo" class="">Usuario</label>
+            <div>
+              <InputText class="p-inputtext-lg w-1/2" v-model="formDataRegister.email" type="text" />
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <label for="correo" class="">Correo</label>
+            <div>
+              <InputText class="p-inputtext-lg w-1/2" v-model="formDataRegister.email" type="text" />
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <label for="password" class="">Contraseña</label>
+            <div>
+              <Password
+                class="p-inputtext-lg"
+                v-model="formDataRegister.password"
+                toggleMask
+                :feedback="false"
+                required
+              />
+            </div>
+          </div>
+          <div class="flex flex-col">
+            <label for="password" class="">Verificar Contraseña</label>
+            <div>
+              <Password
+                class="p-inputtext-lg"
+                v-model="formDataRegister.password"
+                toggleMask
+                :feedback="false"
+                required
+              />
+            </div>
+          </div>
+        </div>
+        <div class="bg-blue-200 ">
+          <button class="bg-[#ccba8d]">jugar</button>
+        </div>
+        </div>
+       
+      </form>
+      </div>
+      
+    </section>
   </body>
 </template>
